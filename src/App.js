03320e6b9d32258a373f9acc3ddae62057168594
Vadym2021 <aisclass@ukr.net>
2022-01-29 15:40:12 +0200

    red02

--- src/App.js
+++ src/App.js
@@ -14,52 +14,66 @@ export const ACTIONS = {
 const App = () => {
 
 
-
-
-    const reducer = (cats, action) => {
+    const reducer = (state, action) => {
         switch (action.type) {
-            // case 'inc':
-            //     return {...state, count1: state.count1 + 1}
-            // case 'dec':
-            //     return {...state, count1: state.count1 - 1}
-            // case 'reset':
-            //     return {...state, count1: 0}
-            // case 'inc2':
-            //     return {...state, count2: state.count2 + 1}
-            // case 'dec2':
-            //     return {...state, count2: state.count2 - 1}
-            // case 'reset2':
-            //     return {...state, count2: 0}
-            // case 'inc3':
-            //     return {...state, count3: state.count3 + 1}
-            // case 'dec4':
-            //     return {...state, count3: state.count3 - 1}
-            // case 'reset3':
-            //     return {...state, count3: 0}
+
+
+            case 'inc':
+                return {...state, count1: state.count1 + 1}
+            case 'dec':
+                return {...state, count1: state.count1 - 1}
+            case 'reset':
+                return {...state, count1: 0}
+            case 'inc2':
+                return {...state, count2: state.count2 + 1}
+            case 'dec2':
+                return {...state, count2: state.count2 - 1}
+            case 'reset2':
+                return {...state, count2: 0}
+            case 'inc3':
+                return {...state, count3: state.count3 + 1}
+            case 'dec4':
+                return {...state, count3: state.count3 - 1}
+            case 'reset3':
+                return {...state, count3: 0}
+
             case ACTIONS.ADD_CAT:
-                return [...cats, newCat(action.payload.cat)]
+                return {...state, cats: [...state.cats, newCat(action.payload.cat)]}
             case ACTIONS.DELETE_CAT:
-                return cats.filter(cat=>cat.id !== action.payload.id)
+                return {...state, cats: [...state.cats.filter(cat => cat.id !== action.payload.id)]}
             case ACTIONS.ADD_DOG:
-                return [...dogs, newDog(action.payload.dog)]
+                return {...state, dogs: [...state.dogs, newDog(action.payload.cat)]}
             case ACTIONS.DELETE_DOG:
-                return dogs.filter(dog=>dog.id !== action.payload.id)
+                return {...state, dogs: [...state.dogs.filter(dog => dog.id !== action.payload.id)]}
+            // case ACTIONS.ADD_CAT:
+            //     return [...state, newCat(action.payload.cat)]
+            // case ACTIONS.DELETE_CAT:
+            //     return state.filter(cat => cat.id !== action.payload.id)
+            // case ACTIONS.ADD_DOG:
+            //     return [...state, newDog(action.payload.dog)]
+            // case ACTIONS.DELETE_DOG:
+            //     return state.filter(dog => dog.id !== action.payload.id)
         }
-        return cats
+        return state
+
     }
 
     function newCat(cat) {
         return {id: Date.now(), cat: cat}
+
     }
+
     function newDog(dog) {
         return {id: Date.now(), dog: dog}
     }
 
 
-
     // const [state, dispatch] = useReducer(reducer, {count1: 0, count2: 0, count3: 0});
 
-    const [cats, dispatch] = useReducer(reducer, []);
+    // const [cats, dispatch] = useReducer(reducer, []);
+
+    const [state, dispatch] = useReducer(reducer, {cats: [], dogs: [], count1: 0, count2: 0, count3: 0});
+
 
     const [cat, setCat] = useState('')
 
@@ -70,29 +84,28 @@ const App = () => {
         dispatch({type: ACTIONS.ADD_CAT, payload: {cat: cat}})
         setCat('')
     }
+
     function handleSubmitDog(e) {
         e.preventDefault()
-        dispatch({type: ACTIONS.ADD_CAT, payload: {cat: cat}})
-        setCat('')
+        dispatch({type: ACTIONS.ADD_DOG, payload: {dog: dog}})
+        setDog('')
     }
-
-    // console.log(todos)
 
 
     return (
         <div>
-            {/*<div>{state.count1}</div>*/}
-            {/*<button onClick={() => dispatch({type: 'inc'})}>Inc</button>*/}
-            {/*<button onClick={() => dispatch({type: 'dec'})}>Dec</button>*/}
-            {/*<button onClick={() => dispatch({type: 'reset', payload: "0"})}>RESET</button>*/}
-            {/*<div>{state.count2}</div>*/}
-            {/*<button onClick={() => dispatch({type: 'inc2'})}>Inc</button>*/}
-            {/*<button onClick={() => dispatch({type: 'dec2'})}>Dec</button>*/}
-            {/*<button onClick={() => dispatch({type: 'reset2', payload: "0"})}>RESET</button>*/}
-            {/*<div>{state.count3}</div>*/}
-            {/*<button onClick={() => dispatch({type: 'inc3', })}>Inc</button>*/}
-            {/*<button onClick={() => dispatch({type: 'dec3', })}>Dec</button>*/}
-            {/*<button onClick={() => dispatch({type: 'reset3', payload: '0'})}>RESET</button>*/}
+            <div>{state.count1}</div>
+            <button onClick={() => dispatch({type: 'inc', payload: 'count1'})}>Inc</button>
+            <button onClick={() => dispatch({type: 'dec'})}>Dec</button>
+            <button onClick={() => dispatch({type: 'reset', payload: "0"})}>RESET</button>
+            <div>{state.count2}</div>
+            <button onClick={() => dispatch({type: 'inc2'})}>Inc</button>
+            <button onClick={() => dispatch({type: 'dec2'})}>Dec</button>
+            <button onClick={() => dispatch({type: 'reset2', payload: "0"})}>RESET</button>
+            <div>{state.count3}</div>
+            <button onClick={() => dispatch({type: 'inc3',})}>Inc</button>
+            <button onClick={() => dispatch({type: 'dec3',})}>Dec</button>
+            <button onClick={() => dispatch({type: 'reset3', payload: '0'})}>RESET</button>
 
             <div>
 
@@ -102,23 +115,23 @@ const App = () => {
                 </form>
             </div>
             <div>
-                {cats.map(cat=>{
-                   return <Cat key={cat.id} cat={cat} dispatch={dispatch}/>
+                {state.cats.map(cat => {
+                    return <Cat key={cat.id} cat={cat} dispatch={dispatch}/>
                 })}
             </div>
 
 
-            {/*<div>*/}
-            {/*    <form onSubmit={handleSubmitDog}>*/}
-            {/*        <input type="text" value={dog} onChange={e => setDog(e.target.value)}/>*/}
-            {/*        <button>Add Dog</button>*/}
-            {/*    </form>*/}
-            {/*</div>*/}
-            {/*<div>*/}
-            {/*    {dogs.map(dog=>{*/}
-            {/*        return <Dog key={dog.id} dog={dog} dispatch={dispatch}/>*/}
-            {/*    })}*/}
-            {/*</div>*/}
+            <div>
+                <form onSubmit={handleSubmitDog}>
+                    <input type="text" value={dog} onChange={e => setDog(e.target.value)}/>
+                    <button>Add Dog</button>
+                </form>
+            </div>
+            <div>
+                {state.dogs.map(dog => {
+                    return <Dog key={dog.id} dog={dog} dispatch={dispatch}/>
+                })}
+            </div>
 
         </div>
     );
